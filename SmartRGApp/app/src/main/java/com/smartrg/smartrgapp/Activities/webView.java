21ee8bb14052bec00614f73dfa4efd4adef2346a
hduransoto <hduransoto@umail.ucsb.edu>
2017-10-19 20:43:46 -0700

    Added a new activity that will display the configuration URL in a WebView Frame

---
+++ SmartRGApp/app/src/main/java/com/smartrg/smartrgapp/Activities/webView.java
@@ -0,0 +1,62 @@
+package com.smartrg.smartrgapp.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.smartrg.smartrgapp.R;
+
+public class webView extends AppCompatActivity {
+
+    //initializing WebView
+    WebView mwebView;
+    //Assign the url to load
+    String urlToLoad = "https://www.google.com/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Init mwebView with WebView Window declared in activity_web_view
+        mwebView = (WebView) findViewById(R.id.myWebView);
+        WebSettings webSettings = mwebView.getSettings();
+        //Tell the WebView to enable JavaScript execution.
+        webSettings.setJavaScriptEnabled(true);
+        //improve webView performance
+        mwebView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+        mwebView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        mwebView.getSettings().setAppCacheEnabled(true);
+        mwebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        webSettings.setUseWideViewPort(true);
+        //Attempt to store user creds for future use
+        webSettings.setSavePassword(true);
+        webSettings.setSaveFormData(true);
+        webSettings.setEnableSmoothTransition(true);
+
+        //force links open in webview only
+        mwebView.setWebViewClient(new WebViewClient() {
+            //Default fundtion is to open URL in browser
+            //We have to override that function
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+        });
+        //Load and open up the URL
+        mwebView.loadUrl(urlToLoad);
+
+
+    }
+
+}
