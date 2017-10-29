9297ad0653d56f33a3c6fa32c2b4146d8253ba3c
Angel Rivera <wrathchild4213@gmail.com>
2017-10-29 01:35:34 -0700

    Implemented threads within the C code, and textview is updated with the returned value

--- SmartRGApp/app/src/main/java/com/smartrg/smartrgapp/Activities/VideoStreamAnalyzerActivity.java
+++ SmartRGApp/app/src/main/java/com/smartrg/smartrgapp/Activities/VideoStreamAnalyzerActivity.java
@@ -5,6 +5,7 @@ import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.Keep;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
@@ -47,6 +48,8 @@ public class VideoStreamAnalyzerActivity extends AppCompatActivity {
 
     private final Handler handler = new Handler();
     private Runnable timer;
+
+    private TextView jniTV;
 
 
     @Override
@@ -141,13 +144,16 @@ public class VideoStreamAnalyzerActivity extends AppCompatActivity {
 
 
         /*Trivial textview used for example to show jni is working*/
-        ((TextView) findViewById(R.id.jnitv)).setText(getVSAString());
+        jniTV = (TextView) findViewById(R.id.jnitv);
+        jniTV.setText(getVSAString());
 
     }
 
     @Override
     public void onResume() {
         super.onResume();
+
+        startVSA();
 
         timer = new Runnable() {
             @Override
@@ -170,7 +176,28 @@ public class VideoStreamAnalyzerActivity extends AppCompatActivity {
     @Override
     public void onPause() {
         handler.removeCallbacks(timer);
+        stopVSA();
         super.onPause();
+    }
+
+    /*
+     * A function calling from JNI to update graphs
+     *      @Keep is used to make sure that during build time this code is not removed because
+     *      it may think it is not used, but NDK uses reflection to access methods and classes.
+     *
+     *      Note: This function still sits on a separate thread, reference file
+     *      app/src/main/jni/VSA-jni.c
+     */
+    @Keep
+    private void updateGraphs(String data) {
+        final String innerData = data;
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                VideoStreamAnalyzerActivity.this.jniTV.setText(innerData);
+            }
+        });
     }
 
     //load shared c library
@@ -178,8 +205,9 @@ public class VideoStreamAnalyzerActivity extends AppCompatActivity {
         System.loadLibrary("VSA-jni");
     }
 
+    //specify native functions that can be called
     public native String getVSAString();
-
-
+    public native void startVSA();
+    public native void stopVSA();
 
 }
